3168d145e5eb7347ef051aa434223af2b66754d5
Justin Martin <lupusdei108@gmail.com>
2011-07-09 17:50:00 -0500

    Updated dependacies to a non SNAPSHOT version of gaeshi.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject limelightmentor "0.0.1"
   :description "A website deployable to AppEngine"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [gaeshi/kake "0.6.0-SNAPSHOT"]]
-  :dev-dependencies [[gaeshi/tsukuri "0.6.0-SNAPSHOT"]
+                 [gaeshi/kake "0.5.0"]]
+  :dev-dependencies [[gaeshi/tsukuri "0.5.0"]
                      [speclj "1.4.0"]]
   :test-path "spec/"
   :java-source-path "src/"
